296785f6497d0c1c3257deafbfd116df4a57f728
Wilson Wong <wilson.shen97@gmail.com>
2024-02-01 16:23:25 +0800

    formatted file

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,4 +1,4 @@
-require'nvim-treesitter.configs'.setup {
+require 'nvim-treesitter.configs'.setup({
   ensure_installed = {
     'c',
     'css',
@@ -26,14 +26,13 @@ require'nvim-treesitter.configs'.setup {
     'vim',
     'vimdoc',
   },
-	sync_install = false,
-	auto_install = true,
-	highlight = {
-		enable = true,
-		additional_vim_regex_highlighting = false,
-	},
+  sync_install = false,
+  auto_install = true,
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
   autotag = {
     enable = true,
   }
-}
-
+})
