b9bede2436aee60c958df22cbe2dd92d2c5308ec
Wilson Wong <wilson.shen97@gmail.com>
2024-04-08 13:16:36 +0800

    added blade treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -9,94 +9,113 @@ return {
       -- Additional text objects for treesitter
       "nvim-treesitter/nvim-treesitter-textobjects",
     },
-    config = function()
-      ---@diagnostic disable: missing-fields
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = {
-          "bash",
-          "c",
-          "css",
-          "dart",
-          "go",
-          "gomod",
-          "gosum",
-          "gowork",
-          "html",
-          "ini",
-          "javascript",
-          "jsdoc",
-          "json",
-          "json5",
-          "lua",
-          "markdown",
-          "php",
-          "phpdoc",
-          "query",
-          "rust",
-          "scss",
-          "sql",
-          "svelte",
-          "tsx",
-          "typescript",
-          "vim",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "c",
+        "css",
+        "dart",
+        "go",
+        "gomod",
+        "gosum",
+        "gowork",
+        "html",
+        "ini",
+        "javascript",
+        "jsdoc",
+        "json",
+        "json5",
+        "lua",
+        "markdown",
+        "php",
+        "php_only",
+        "phpdoc",
+        "query",
+        "rust",
+        "scss",
+        "sql",
+        "svelte",
+        "tsx",
+        "typescript",
+        "vim",
+      },
+      sync_install = false,
+      highlight = {
+        enable = true,
+      },
+      indent = {
+        enable = true,
+      },
+      autopairs = {
+        enable = true,
+      },
+      autotag = {
+        enable = true,
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<c-space>",
+          node_incremental = "<c-space>",
+          scope_incremental = "<c-s>",
+          node_decremental = "<c-backspace>",
         },
-        sync_install = false,
-        highlight = {
+      },
+      textobjects = {
+        select = {
           enable = true,
-        },
-        indent = {
-          enable = true,
-        },
-        autopairs = {
-          enable = true,
-        },
-        autotag = {
-          enable = true,
-        },
-        incremental_selection = {
-          enable = true,
+          lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
           keymaps = {
-            init_selection = "<c-space>",
-            node_incremental = "<c-space>",
-            scope_incremental = "<c-s>",
-            node_decremental = "<c-backspace>",
+            ["aa"] = "@parameter.outer",
+            ["ia"] = "@parameter.inner",
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["ac"] = "@class.outer",
+            ["ic"] = "@class.inner",
           },
         },
-        textobjects = {
-          select = {
-            enable = true,
-            lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-            keymaps = {
-              ["aa"] = "@parameter.outer",
-              ["ia"] = "@parameter.inner",
-              ["af"] = "@function.outer",
-              ["if"] = "@function.inner",
-              ["ac"] = "@class.outer",
-              ["ic"] = "@class.inner",
-            },
+        move = {
+          enable = true,
+          set_jumps = true, -- whether to set jumps in the jumplist
+          goto_next_start = {
+            ["]m"] = "@function.outer",
+            ["]]"] = "@class.outer",
           },
-          move = {
-            enable = true,
-            set_jumps = true, -- whether to set jumps in the jumplist
-            goto_next_start = {
-              ["]m"] = "@function.outer",
-              ["]]"] = "@class.outer",
-            },
-            goto_next_end = {
-              ["]M"] = "@function.outer",
-              ["]["] = "@class.outer",
-            },
-            goto_previous_start = {
-              ["[m"] = "@function.outer",
-              ["[["] = "@class.outer",
-            },
-            goto_previous_end = {
-              ["[M"] = "@function.outer",
-              ["[]"] = "@class.outer",
-            },
+          goto_next_end = {
+            ["]M"] = "@function.outer",
+            ["]["] = "@class.outer",
           },
+          goto_previous_start = {
+            ["[m"] = "@function.outer",
+            ["[["] = "@class.outer",
+          },
+          goto_previous_end = {
+            ["[M"] = "@function.outer",
+            ["[]"] = "@class.outer",
+          },
+        },
+      },
+    },
+    config = function(_, opts)
+      local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+
+      ---@diagnostic disable-next-line: inject-field
+      parser_config.blade = {
+        install_info = {
+          url = "https://github.com/EmranMR/tree-sitter-blade",
+          files = { "src/parser.c" },
+          branch = "main",
+        },
+        filetype = "blade",
+      }
+
+      vim.filetype.add({
+        pattern = {
+          [".*%.blade%.php"] = "blade",
         },
       })
+
+      require("nvim-treesitter.configs").setup(opts)
     end,
   },
 }
