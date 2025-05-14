1e39fc123c7a04c8ce22d58f23b748223c716d47
wilson-shen <wilson.shen97@gmail.com>
2025-05-14 20:42:08 +0800

    fixed double alpha shown

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -9,6 +9,7 @@ return {
   {
     "stevearc/oil.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
+    event = { "VeryLazy" },
     config = function()
       local oil = require("oil")
 
